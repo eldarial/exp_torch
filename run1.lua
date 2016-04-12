6c0ed8fae0211c6697aabdfef489321053c42861
eldarial <daltamar@stanford.edu>
2016-04-12 11:55:22 -0500

    added function to extract loss and gradients

--- run1.lua
+++ run1.lua
@@ -5,8 +5,9 @@ require 'string'
 npy4th = require 'npy4th'
 
 -- adjust size
-batch_size = 10
+batch_size = 2
 num_classes = 9
+pixels_image = 112*112
 
 -- function to load data
 function load_specific_data(index_file)
@@ -34,6 +35,9 @@ print("** information of batch **")
 print(train_data:size())
 print(train_label:size())
 
+y_data = nn.Reshape(batch_size*112*112, num_classes):forward(nn.Transpose({2,4}):forward(train_label))
+print(y_data:size())
+
 -- create network
 model = nn.Sequential()
 model:add(nn.SpatialConvolution(1, 32, 9, 9))
@@ -54,12 +58,36 @@ model:add(nn.ReLU())
 model:add(nn.SpatialFullConvolution(1024, num_classes, 14, 14, 14, 14))
 model:add(nn.Transpose({2,4}))
 model:add(nn.Reshape(batch_size*112*112, num_classes))
-fwd1 = model:forward(train_data:double())
+
+x, dl_dx = model:getParameters()
+
 model:add(nn.LogSoftMax())
-fwd2 = model:forward(train_data:double())
+criterion = nn.ClassNLLCriterion()
+
+
+local feval = function(x_data)
+	dl_dx:zero()
+	copy_dl = dl_dx:clone()
+	print(torch.all(copy_dl:eq(dl_dx)))
+	local f = 0
+	print("****")
+	local df_do = torch.Tensor(batch_size*112*112, num_classes)
+	local output = model:forward(x_data:double())
+	for i=1,batch_size do
+		df_do[{i}] = criterion:backward(output[{i}], y_data[{i}])
+		--df_do = criterion:backward(output, y_data[{{(i-1)*pixels_image + 1,i*pixels_image}}])
+		print(output:size())
+		print(df_do:size())
+	end
+	model:backward(x_data, df_do)
+	print(torch.all(copy_dl:eq(dl_dx)))
+	return f, dl_dx
+end
+
+feval(train_data)
+print("pass function")
+
 
 
 -- test spatial convolutions
 print("outputs size")
-print(fwd1[{1,{1,3}}])
-print(fwd2[{1,{1,3}}])
